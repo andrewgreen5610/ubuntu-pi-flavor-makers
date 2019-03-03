f56d4502440aa4587e11dc25eb309fd65a05024a
Martin Wimpress <code@flexion.org>
2019-03-03 12:25:25 +0000

    EGL/GLES/OpenVG libraries for VideoCore IV now available for arm64

--- build-image.sh
+++ build-image.sh
@@ -347,10 +347,7 @@ function configure_hardware() {
     fi
 
     # Install miscellaneous Raspberry Pi utilities EGL/GLES/OpenVG libraries for VideoCore IV
-    # Currently only available for armhf
-    if [ "${ARCHITECTURE}" == "armhf" ]; then
-        nspawn apt-get - y install libraspberrypi-bin libraspberrypi0
-    fi
+    nspawn apt-get - y install libraspberrypi-bin libraspberrypi-bin-nonfree libraspberrypi0
 
     # Create symlinks for config.txt and cmdline.txt in familiar places.
     nspawn ln -s /boot/firmware/config.txt /boot/
