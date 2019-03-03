e10e3298780d0e8856cc053bd4e7635bb8c074d8
Martin Wimpress <code@flexion.org>
2019-03-03 19:33:06 +0000

    Update publish script.

--- publish-image.sh
+++ publish-image.sh
@@ -21,7 +21,7 @@
 
 set -ex
 
-DEST="yor"
+DEST="man"
 
 if [ -f build-settings.sh ]; then
     source build-settings.sh
@@ -37,18 +37,18 @@ fi
 
 function publish_image() {
     echo "Sending to: ${DEST}"
-    ssh matey@${DEST}.ubuntu-mate.net mkdir -p /home/matey/ISO-Mirror/${RELEASE}/armhf/
-    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
-    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
-    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+    ssh matey@${DEST}.ubuntu-mate.net mkdir -p /home/matey/ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
 }
 
 function publish_tarball() {
     if [ ${MAKE_TARBALL} -eq 1 ]; then
         echo "Sending to: ${DEST}"
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/${ARCHITECTURE}/
     fi
 }
 
