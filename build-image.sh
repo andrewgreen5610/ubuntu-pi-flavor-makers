06c4948625e13ecb7ffbea26a8861116b148be1e
Martin Wimpress <code@flexion.org>
2019-03-04 23:34:17 +0000

    Fix preseeding snaps.

--- build-image.sh
+++ build-image.sh
@@ -285,37 +285,42 @@ function disable_services() {
 # Adapted from:
 #  * https://blackboxsw.github.io/seed-snaps-using-maas.html
 function seed_snaps() {
-    local SEED_DIR="${R}/var/lib/snapd/seed"
+    local SEED_DIR="/var/lib/snapd/seed"
     local SEED_CHANNEL="stable/ubuntu-${REL_VER}"
     local SEED_SNAPS="${STRICT_SNAPS} ${CLASSIC_SNAPS}"
 
     if [ -n "${STRICT_SNAPS}" ] || [ -n "${CLASSIC_SNAPS}" ]; then
         # Preseeded snaps should be downloaded from a versioned channel
-        mkdir -p "${SEED_DIR}/snaps"
-        mkdir -p "${SEED_DIR}/assertions"
+        nspawn rm -rfv "${SEED_DIR}"
+        nspawn mkdir -p "${SEED_DIR}/snaps"
+        nspawn mkdir -p "${SEED_DIR}/assertions"
 
         # Download the published snaps and their related assert files
-        # core snap is required for seeding
-        cd "${SEED_DIR}/snaps"
+        # Runs inside the contiainer
         for SEED_SNAP in ${SEED_SNAPS}; do
-        if [ "${SEED_SNAP}" == "core" ] || [ "${SEED_SNAP}" == "core16" ] || [ "${SEED_SNAP}" == "core18" ]; then
-            snap download --channel=stable "${SEED_SNAP}"
-        else
-            snap download --channel="${SEED_CHANNEL}" "${SEED_SNAP}"
-        fi
-        mv "${SEED_SNAP}*.assert" ../assertions
+            if [ "${SEED_SNAP}" == "core" ] || [ "${SEED_SNAP}" == "core16" ] || [ "${SEED_SNAP}" == "core18" ]; then
+                nspawn snap download --channel=stable "${SEED_SNAP}"
+            else
+                nspawn snap download --channel="${SEED_CHANNEL}" "${SEED_SNAP}"
+            fi
         done
-        cd -
+
+        # Move snaps and seertiosn to the correct place
+        # Runs outside the container.
+        mv -v ${R}/*.snap ${R}/${SEED_DIR}/snaps/
+        mv -v ${R}/*.assert ${R}/${SEED_DIR}/assertions/
 
         # Create model and account assertions
-        snap known --remote model series=16 model=generic-classic brand-id=generic > ${SEED_DIR}/assertions/generic-classic.model
-        ACCOUNT_KEY=$(grep "^sign-key-sha3-384" ${SEED_DIR}/assertions/generic-classic.model | cut -d':' -f2 | sed 's/ //g')
-        snap known --remote account-key public-key-sha3-384=${ACCOUNT_KEY} > ${SEED_DIR}/assertions/generic.account-key
-        snap known --remote account account-id=generic > ${SEED_DIR}/assertions/generic.account
+        # Runs inside the container.
+        snap known --remote model series=16 model=generic-classic brand-id=generic > ${R}/${SEED_DIR}/assertions/generic-classic.model
+        ACCOUNT_KEY=$(grep "^sign-key-sha3-384" ${R}/${SEED_DIR}/assertions/generic-classic.model | cut -d':' -f2 | sed 's/ //g')
+        snap known --remote account-key public-key-sha3-384=${ACCOUNT_KEY} > ${R}/${SEED_DIR}/assertions/generic.account-key
+        snap known --remote account account-id=generic > ${R}/${SEED_DIR}/assertions/generic.account
 
         # Create the seed.yaml: the manifest of snaps to install
-        echo "snaps:" > ${SEED_DIR}/seed.yaml
-        for ASSERT_FILE in ${SEED_DIR}/assertions/*.assert; do
+        # Runs outside the container
+        echo "snaps:" > ${R}/${SEED_DIR}/seed.yaml
+        for ASSERT_FILE in ${R}/${SEED_DIR}/assertions/*.assert; do
             SNAP_NAME=$(grep "^snap-name" ${ASSERT_FILE} | cut -d':' -f2 | sed 's/ //g')
             SNAP_REVISION=$(grep "^snap-revision" ${ASSERT_FILE} | cut -d':' -f2 | sed 's/ //g')
             if [ "${SNAP_NAME}" == "core" ] || [ "${SNAP}" == "core16" ] || [ "${SNAP}" == "core18" ]; then
@@ -326,12 +331,12 @@ function seed_snaps() {
 
             # Classic snaps require a "classic: true" attribute in the seed file
             if [[ $CLASSIC_SNAPS =~ $SNAP_NAME ]]; then
-                printf " - name: %s\n   channel: %s\n   classic: true\n   file: %s_%s.snap\n" ${SNAP_NAME} ${SNAP_CHANNEL} ${SNAP_NAME} ${SNAP_REVISION} >> ${SEED_DIR}/seed.yaml
+                printf " - name: %s\n   channel: %s\n   classic: true\n   file: %s_%s.snap\n" ${SNAP_NAME} ${SNAP_CHANNEL} ${SNAP_NAME} ${SNAP_REVISION} >> ${R}/${SEED_DIR}/seed.yaml
             else
-                printf " - name: %s\n   channel: %s\n   file: %s_%s.snap\n" ${SNAP_NAME} ${SNAP_CHANNEL} ${SNAP_NAME} ${SNAP_REVISION} >> ${SEED_DIR}/seed.yaml
+                printf " - name: %s\n   channel: %s\n   file: %s_%s.snap\n" ${SNAP_NAME} ${SNAP_CHANNEL} ${SNAP_NAME} ${SNAP_REVISION} >> ${R}/${SEED_DIR}/seed.yaml
             fi
         done
-        cat ${SEED_DIR}/seed.yaml
+        cat ${R}/${SEED_DIR}/seed.yaml
     fi
 }
 
@@ -576,7 +581,7 @@ function stage_02_desktop() {
         for META_PACKAGE in ${META_PACKAGES}; do
             install_meta "${META_PACKAGE}"
         done
-
+        seed_snaps
         create_groups
         create_user
         prepare_oem_config
