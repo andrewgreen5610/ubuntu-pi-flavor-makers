368e11066521dc0b90b866537f5956f3f95ed3a5
Martin Wimpress <code@flexion.org>
2019-03-04 23:45:07 +0000

    Update build settings

--- build-settings-ubuntu-mate.sh
+++ build-settings-ubuntu-mate.sh
@@ -27,13 +27,25 @@ MIN_VER="04"
 PNT_VER="2"
 REL_VER="${MAJ_VER}.${MIN_VER}"
 VERSION="${REL_VER}.${PNT_VER}"
-QUALITY="-alpha"
+QUALITY="-alpha2"
 ARCHITECTURE="armhf"
 ENABLE_VC4=0
 META_PACKAGES="ubuntu-mate-core ubuntu-mate-desktop"
+STRICT_SNAPS="core pulsemixer"
+CLASSIC_SNAPS="ubuntu-mate-welcome software-boutique"
 
 # Either 'ext4' or 'f2fs'
 FS_TYPE="ext4"
+
+# Either 0 or 1.
+# - 0 don't make generic rootfs tarball
+# - 1 make a generic rootfs tarball
+MAKE_TARBALL=1
+
+# Choose to use OEM setup or hardcode a pre-installed user.
+# - 0 hardcode a user. username and password will $FLAVOUR/$FLAVOUR
+# - 1 use oem-config. username and password for OEM the session are oem/oem
+OEM_CONFIG=1
 
 # Target image size, will be represented in GB
 if [ "${FS_TYPE}" == "ext4" ]; then
@@ -48,11 +60,6 @@ elif [ "${ARCHITECTURE}" == "arm64" ]; then
     SUB_ARCH="raspi3"
 fi
 
-# Either 0 or 1.
-# - 0 don't make generic rootfs tarball
-# - 1 make a generic rootfs tarball
-MAKE_TARBALL=1
-
 TARBALL="${FLAVOUR}-${VERSION}${QUALITY}-desktop-${ARCHITECTURE}+${SUB_ARCH}.tar.xz"
 IMAGE="${FLAVOUR}-${VERSION}${QUALITY}-desktop-${ARCHITECTURE}+${SUB_ARCH}-${FS_TYPE}.img"
 BASEDIR=${HOME}/PiFlavourMaker/${RELEASE}/${ARCHITECTURE}
@@ -61,8 +68,3 @@ BASE_R=${BASEDIR}/base
 DESKTOP_R=${BUILDDIR}/desktop
 DEVICE_R=${BUILDDIR}/pi
 export TZ=UTC
-
-# Override OEM_CONFIG here if required. Either 0 or 1.
-# - 0 hardcode a user. username and password will $FLAVOUR/$FLAVOUR
-# - 1 use oem-config. username and password for OEM the session are oem/oem
-OEM_CONFIG=1
